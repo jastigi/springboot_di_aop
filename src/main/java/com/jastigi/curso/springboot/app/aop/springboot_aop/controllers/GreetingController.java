055e33f7e45a15ef9c9a8f18cb9f1490e1a1d2e0
jastigi <jastigi@gmail.com>
2026-01-20 10:45:51 +0100

    Service

--- src/main/java/com/jastigi/curso/springboot/app/aop/springboot_aop/controllers/GreetingController.java
+++ src/main/java/com/jastigi/curso/springboot/app/aop/springboot_aop/controllers/GreetingController.java
@@ -2,17 +2,24 @@ package com.jastigi.curso.springboot.app.aop.springboot_aop.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jastigi.curso.springboot.app.aop.springboot_aop.services.GreetingService;
+
 import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class GreetingController {
 
+    @Autowired
+    private GreetingService greetingService;
+
     @GetMapping("/greeting")
     public ResponseEntity<?> greeting() {
-        return ResponseEntity.ok(Collections.singletonMap("greeting", "Hello World"));
+        return ResponseEntity
+                .ok(Collections.singletonMap("greeting", greetingService.sayHello("Jhon", "Good Morning")));
     }
 
 }

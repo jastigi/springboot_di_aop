4c55e862e97812bc302354e4088ff6a17348e325
jastigi <jastigi@gmail.com>
2026-01-22 11:05:49 +0100

    Ordenando Aspectos

---
+++ src/main/java/com/jastigi/curso/springboot/app/aop/aop/GreetingFooAspect.java
@@ -0,0 +1,40 @@
+package com.jastigi.curso.springboot.app.aop.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(1)
+@Component
+@Aspect
+public class GreetingFooAspect {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Before("execution(* com.jastigi.curso.springboot.app.aop.services.GreetingService.*(..))")
+    public void loggerBefore(JoinPoint joinPoint) {
+
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        logger.info("Antes Foo: " + method + " invocado con los parametros " + args);
+
+    }
+
+    @After("execution(* com.jastigi.curso.springboot.app.aop.services.GreetingService.*(..))")
+    public void loggerAfter(JoinPoint joinPoint) {
+
+        String method = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+
+        logger.info("Despues Foo: " + method + " invocado con los parametros " + args);
+
+    }
+
+}
